642a11bb78421f25a28cae6793760137f5cd2efc
LordlingZhang <852611546@qq.com>
2018-06-30 18:01:33 +0800

    提交

---
+++ src/main/java/simplefactory/Sender.java
@@ -0,0 +1,20 @@
+/**
+ * Copyright (C), 2002-2018, 苏宁易购电子商务有限公司
+ * FileName: Sender
+ * Author:   17020644
+ * Date:     2018/06/30 17:56
+ * Description: 发送接口
+ */
+package simplefactory;
+
+/**
+ * @description: 发送接口
+ * @author: 17020644
+ */
+public interface Sender {
+    /**
+     * 发送
+     */
+    void send();
+
+}
