d99e9581856e254268c2a5b611c0a7c0e0c43ea9
Princess_Chao <1205595310@qq.com>
2018-07-02 22:21:02 +0800

    提交合并

--- src/main/java/factorymethod/SendSmsFactoy.java
+++ src/main/java/factorymethod/SendSmsFactoy.java
@@ -5,7 +5,7 @@ import simplefactory.SmsSender;
 
 /**
  * @author : Lordling_Zhang
- * @description : ${描述}
+ * @description : 短信发送工厂
  * @date : 2018/7/1
  */
 public class SendSmsFactoy implements Provider{
--- src/main/java/simplefactory/ManyStaticSimpleFactory.java
+++ src/main/java/simplefactory/ManyStaticSimpleFactory.java
@@ -2,8 +2,31 @@ package simplefactory;
 
 /*
  * @author : Lordling_Zhang
- * @description : ${描述}
+ * @description : 多个静态工厂
  * @date : 2018/7/1
  */
 public class ManyStaticSimpleFactory {
+    /**
+     * 生产短信发送
+     * @return
+     */
+    public static Sender produceSmsSender(){
+        return new SmsSender();
+    }
+
+    /**
+     * 生产邮件发送
+     * @return
+     */
+    public static Sender produceMailSender(){
+        return new MailSender();
+    }
+
+
+
+    public static void main(String[] args){
+        Sender sender = ManyStaticSimpleFactory.produceMailSender();
+        sender.send();
+    }
+
 }
