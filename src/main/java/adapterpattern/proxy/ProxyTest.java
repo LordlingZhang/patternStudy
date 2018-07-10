48612ff48a78e2b2ced4c97bc9d7234498d32411
Princess_Chao <1205595310@qq.com>
2018-07-10 21:57:03 +0800

    代理模式

---
+++ src/main/java/adapterpattern/proxy/ProxyTest.java
@@ -0,0 +1,9 @@
+package adapterpattern.proxy;
+
+/*
+ * @author : Lordling_Zhang
+ * @description : ${描述}
+ * @date : 2018/7/10
+ */
+public class ProxyTest {
+}
---
+++ src/main/java/adapterpattern/proxy/Sourceable1.java
@@ -0,0 +1,15 @@
+package adapterpattern.interfaceadapter;/*
+ * @author : Lordling_Zhang
+ * @description : 初识接口，拥有许多方法
+ * @date : 2018/7/7
+ */
+
+public interface Sourceable {
+
+    void method1();
+
+    void method2();
+
+    void method3();
+
+}
